8b83a306a51191c03661117ab01f152a994f1ebe
lins <1300957982@qq.com>
2023-06-11 13:04:48 +0800

    修改了命令行bug

--- com/pj/chess/ChessBoardMain.java
+++ com/pj/chess/ChessBoardMain.java
@@ -864,10 +864,10 @@ public class ChessBoardMain extends JFrame
 //                requestBoard = "rnbakabnr/9/1c5c1/p1p1p1p1p/9/9/P1P1P1P1P/1CN4C1/9/R1BAKABNR b";
                 requestBoard = requestBoard + Tools.toFEN(chessParamCont.board, moveHistory) + (play == 0 ? " b" : " w");
                 System.out.println(requestBoard);
+                processEngine.getInformation(requestBoard+"\ngo depth 5");
+                //String requestCmd = "go infinite";
+                //processEngine.getInformation(requestCmd);
 //                processEngine.getInformation(requestBoard);
-                String requestCmd = "bench";
-                processEngine.getInformation(requestCmd);
-                processEngine.getInformation(requestCmd);
 //                String run = null;
 //                try {
 //                    run = api.run("http://www.chessdb.cn/chessdb.php?action=querybest&board=" + requestBoard);
